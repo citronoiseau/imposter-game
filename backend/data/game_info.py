24e9adb9f1e7c32cc8f20e7fdbbd8506cffc48a1
citronoiseau <avroraka2004@gmail.com>
2025-09-07 18:28:28 -0700

    added game_state + function to switch them

--- backend/data/game_info.py
+++ backend/data/game_info.py
@@ -3,7 +3,6 @@ from dataclasses import field
 from enum import Enum
 
 class GameState(str, Enum):
-    SETUP = "SETUP"
     LOBBY = "LOBBY"
     QUESTION = "QUESTION"
     VOTING = "VOTING"
@@ -16,6 +15,41 @@ class GameInfo:
     imposters: int
     inn_question: str
     imp_question: str
+    current_state: GameState = GameState.LOBBY
+
+    def answers_submitted(self) -> bool:
+        return all(player.answer_submitted for player in self.players.values())
+
+    def votes_submitted(self) -> bool:
+        return all(player.vote_submitted for player in self.players.values())
+
+    def next_state(self, host_override=False) -> GameState:
+        """
+        Move to the next state if conditions are met.
+        """
+        if self.current_state == GameState.LOBBY:
+                self.current_state = GameState.QUESTION
+        
+        elif self.current_state == GameState.QUESTION:
+            if self.answers_submitted():
+                self.current_state = GameState.VOTING
+        
+        elif self.current_state == GameState.VOTING:
+             if self.votes_submitted():
+                self.current_state = GameState.RESULTS
+
+        elif self.current_state == GameState.RESULTS:
+            self.new_round()
+            self.current_state = GameState.LOBBY
+
+        return self.current_state
+    
+    def new_round(self):
+        """Reset per-round values for a new round."""
+        for player in self.players.values():
+            player.is_imposter = False
+            player.answer_submitted = False
+            player.vote_submitted = False
 
 @dataclass
 class Player:
@@ -23,3 +57,5 @@ class Player:
     name: str
     is_imposter: bool 
     sid: str
+    answer_submitted: bool = False
+    vote_submitted: bool = False
